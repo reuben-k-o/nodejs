e65e8671a0a6478aaadb7ca7fba12dab895ffa13
Reuben Khaemba <rubenkhaemba@gmail.com>
2021-02-13 23:36:02 +0300

    Commited recently

--- app.js
+++ app.js
@@ -1,25 +1,15 @@
 const http = require('http');
-const fs = require('fs');
 
 const server = http.createServer((req,res) => {
     //console.log(req.url, req.method, req.headers);
 
     const url = req.url;
-    const method = req.method;
     if (url === '/'){
         res.write('<html>');
         res.write('<head><title>Enter the Message </title><head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end(); 
-    }
-    if (url === '/message' && method === 'POST' ){
-        fs.writeFileSync('message.txt', 'DUMMY');
-
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-
     }
 
     res.setHeader('Content-Type', 'text/html');
@@ -30,4 +20,4 @@ const server = http.createServer((req,res) => {
     res.end();
 });
 
- server.listen(3000);
+server.listen(3000);
